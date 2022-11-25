de79f87709c0be261f5c832400d19a5df32236b1
seunghyun <h_sl@hanmail.net>
2022-11-25 12:36:53 +0900

    toDo List part One

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState, useEffect } from "react";
+
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(toDo);
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
+  console.log(toDos);
   return (
     <div>
-      <h1>Welcome back!</h1>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          text="text"
+          placeholer="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
-
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+// import PorpTypes from "prop-types";
+// import styled from "./Button.module.css";
+
+// function Button({ text }) {
+//   return <button className={styled.Btn}>{text}</button>;
+// }
+
+// // Button.propTypes = {
+// //   text: PropTypes.string.isRequired,
+// // };
+
+// export default Button;
