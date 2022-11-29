60f85e622e1c4e9ae60cb5a3f6a993c2435095e9
seunghyun <h_sl@hanmail.net>
2022-11-29 10:39:53 +0900

    Publishing

--- src/App.js
+++ src/App.js
@@ -1,31 +1,14 @@
-import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <string>Loading...</string>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option key={coin.id}>
-              {coin.name} ({coin.symbol} : ${coin.quotes.USD.price} USD)
-            </option>
-          ))}
-        </select>
-      )}
-    </div>
+    <Routes>
+      <Route path="movie/:id" element={<Detail />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   );
 }
+
 export default App;
